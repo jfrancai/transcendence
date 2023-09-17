2036d76d47674607eb75372fcc6c133f2c26e4c4
Julien FRANCAIS <jfrancai@student.42.fr>
2023-09-17 16:26:14 +0200

    removing MessageStoreModule

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
-import MessageStoreModule from './message-store/message-store.module';
 import ChatGateway from './chat.gateway';
 
 @Module({
   controllers: [],
   providers: [ChatGateway],
-  imports: [ConfigModule, MessageStoreModule, AuthModule, DatabaseModule]
+  imports: [ConfigModule, AuthModule, DatabaseModule]
 })
 export default class ChatModule {}
