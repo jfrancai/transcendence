ffaca63b9d73f19c1f524b989e64c5267435801b
qbornet <ring0.csgo@gmail.com>
2023-09-28 18:06:20 +0200

    norm and remove useless file

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -3,39 +3,38 @@ interface UserProfile {
   email: string;
 }
 
+// eslint-next-line-disable
 interface ProfileProps {
   user: UserProfile;
 }
 
-const Profile: React.FC<ProfileProps> = ({ user }) => {
+export default function Profile() {
   return (
     <div className="flex h-screen flex-none flex-col items-center justify-center">
       <div className="flex h-1/2 flex-none grow-0 items-end rounded-[20px] bg-[url('./images/background_profile.png')] bg-cover bg-center">
-        <div class="flex flex-none rounded-[20px] bg-blue-950 bg-opacity-90 p-7 shadow-lg overflow-visible">
-          <p class="grid grid-cols-10 gap-1 text-center text-slate-200">
-            <p class="grid grid-cols-1 text-center">
-              <p class="text-gray-500 font-bold text-sm">Total Game</p>
-              <p class="text-white-800">1242</p>
+        <div className="flex flex-none overflow-visible rounded-[20px] bg-blue-950 bg-opacity-90 p-7 shadow-lg">
+          <p className="grid grid-cols-10 gap-1 text-center text-slate-200">
+            <p className="grid grid-cols-1 text-center">
+              <p className="text-sm font-bold text-gray-500">Total Game</p>
+              <p className="text-white-800">1242</p>
             </p>
-            <p class="text-4xl font-[10] text-gray-500">|</p>
-            <p class="grid grid-cols-1 text-center">
-              <p class="text-gray-500 font-bold text-sm">Win Rate</p>
-              <p class="text-white-800">65%</p>
+            <p className="text-4xl font-[10] text-gray-500">|</p>
+            <p className="grid grid-cols-1 text-center">
+              <p className="text-sm font-bold text-gray-500">Win Rate</p>
+              <p className="text-white-800">65%</p>
             </p>
-            <p class="text-4xl font-[10] text-gray-500">|</p>
-            <p class="grid grid-cols-1 text-center">
-              <p class="text-gray-500 font-bold text-sm">Lose Rate</p>
-              <p class="text-white-800">35%</p>
+            <p className="text-4xl font-[10] text-gray-500">|</p>
+            <p className="grid grid-cols-1 text-center">
+              <p className="text-sm font-bold text-gray-500">Lose Rate</p>
+              <p className="text-white-800">35%</p>
             </p>
-            <p class="Location col-start-10 grid grid-cols-1 text-center">
-              <p class="text-gray-500 font-bold text-sm">Location</p>
-              <p class="text-white-800">France</p>
+            <p className="Location col-start-10 grid grid-cols-1 text-center">
+              <p className="text-sm font-bold text-gray-500">Location</p>
+              <p className="text-white-800">France</p>
             </p>
           </p>
         </div>
       </div>
     </div>
   );
-};
-
-export default Profile;
+}
