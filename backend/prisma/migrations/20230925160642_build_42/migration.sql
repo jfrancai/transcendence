eedf8263e3b4f8a95f8fbce0b2b71c906689d547
Julien FRANCAIS <jfrancai@student.42.fr>
2023-09-29 14:24:20 +0200

    trash commit

--- backend/prisma/migrations/20230925160642_build_42/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `reason` on table `ChanRestrict` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "ChanRestrict" ALTER COLUMN "reason" SET NOT NULL;
